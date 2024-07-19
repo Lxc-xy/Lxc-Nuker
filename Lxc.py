0e94c7f1d595b264141f06df31fcf45df6c14e1e
Lxc-xy <imlxc69@gmail.com>
2024-07-19 20:42:30 +0530

    Lxc.py

--- Lxc.py
+++ Lxc.py
@@ -1,25 +1,5 @@
 """
-MIT License
-
-Copyright (c) 2022 speezy
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+yapping here
 """
 
 import threading, webbrowser, discord, random, httpx, json, time, os; from discord.ext import commands;from itertools import cycle
@@ -32,13 +12,13 @@ __proxies__, __client__, __config__, __threads__= cycle(open("proxies.txt", "r")
 token = __config__["token"]
 os.system("cls") if os.name == "nt" else os.system("clear")
 
-x444_art = """
+lxc_art = """
                                           {}__ __  __ __  __ __
-                                    {}_  __/ // / / // / / // /
+                                    {}
                                    {}| |/_/ // /_/ // /_/ // /_
-                                  {}_>  </__  __/__  __/__  __/
+                                  {}_>  </__  Niggeroid
                                   {}/_/|_|  /_/    /_/    /_/{}
-                               *annihilating faggs is our weapon*
+                               *annihilating niggers is our weapon*
                               ═══════════════════════════════════         
                          ═════════════════════════════════════════════
 """.format("\x1b[38;5;17m", "\x1b[38;5;18m", "\x1b[38;5;19m", "\x1b[38;5;20m", "\x1b[38;5;21m", "\x1b[0m")
